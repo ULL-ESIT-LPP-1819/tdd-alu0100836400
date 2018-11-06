86d57f51a050eb57de79cc5f27768663f00dc529
Javier Alberto <alu0100836400@ull.edu.es>
2018-11-06 23:23:43 +0000

    Pruebas pasan

--- alimento/lib/alimento/etiqueta.rb
+++ alimento/lib/alimento/etiqueta.rb
@@ -6,13 +6,5 @@ class Etiqueta
         @nombre, @grasas, @grasas_saturadas, @hidratos, @azucares, @proteinas, @sal = 
         nombre, grasas, grasas_saturadas, hidratos, azucares, proteinas, sal
     end
-    
-    def calcular_valor_energetico_julios()
-        @valor_energeticoJ = 37*(@grasas + @grasas_saturadas) + 17*@hidratos + 17*@azucares + 17*@proteinas + 25*@sal
-    end
-    
-    def calcular_valor_energetico_cal()
-        @valor_energeticoCal = 37*(@grasas + @grasas_saturadas) + 17*@hidratos + 17*@azucares + 17*@proteinas + 25*@sal
-    end
-    
+
 end
