420b188cda377d2efb89c82450bf44470a637c4a
Javier Alberto <alu0100836400@ull.edu.es>
2018-11-06 23:32:20 +0000

    Pruebas getter fallan

--- spec/etiqueta_spec.rb
+++ spec/etiqueta_spec.rb
@@ -36,4 +36,34 @@ describe Etiqueta do
             expect(@etiqueta.sal).to eq(6)
         end
     end
+    
+    describe "Existe método para obtener" do
+        it "Nombre" do
+            expect(@etiqueta.getNombre).to eq("Leche")
+        end
+        
+        it "Grasas" do
+            expect(@etiqueta.getGrasas).to eq(5)
+        end
+        
+        it "Grasas saturadas" do
+            expect(@etiqueta.getGrasas_saturadas).to eq(8)
+        end
+        
+        it "Hidratos" do
+            expect(@etiqueta.getHidratos).to eq(32)
+        end
+        
+        it "Azúcares" do
+            expect(@etiqueta.getAzucares).to eq(12)
+        end
+        
+        it "Proteínas" do
+            expect(@etiqueta.getProteinas).to eq(7)
+        end
+        
+        it "Sal" do
+            expect(@etiqueta.getSal).to eq(6)
+        end
+    end
 end
