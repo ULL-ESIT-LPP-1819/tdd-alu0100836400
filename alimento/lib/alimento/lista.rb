479a6d75f701f999ed262e26dd12524a392f5548
Javier Alberto <alu0100836400@ull.edu.es>
2018-11-13 20:42:46 +0000

    Se puede insertar elementos en la lista. Pasa

--- alimento/lib/alimento/lista.rb
+++ alimento/lib/alimento/lista.rb
@@ -1,4 +1,45 @@
 Nodo = Struct.new(:valor, :anterior, :siguiente)
 
 class Lista
+    
+    attr_reader :cabeza, :cola
+    
+    def initialize()
+        @cabeza = nil
+        @cola = nil
+    end
+    
+    def insertar_cabeza(dato)
+        @nuevo = Nodo.new(nil, nil, nil)
+        if dato.class == Nodo
+            @nuevo.valor = dato.valor
+        else
+            @nuevo.valor = dato
+        end
+        
+        if @cola == nil
+            @cola = @nuevo
+        else
+            @nuevo.anterior = @cabeza
+            @cabeza.siguiente = @nuevo
+        end
+        @cabeza = @nuevo
+    end
+    
+    def insertar_cola(dato)
+        @nuevo = Nodo.new(nil, nil, nil)
+        if dato.class == Nodo
+            @nuevo.valor = dato.valor
+        else
+            @nuevo.valor = dato
+        end
+        
+        if @cola == nil
+            @cabeza = @nuevo
+        else
+            @nuevo.siguiente = @cola
+            @cola.anterior = @nuevo
+        end
+        @cola = @nuevo
+    end
 end
